63fe893e557ea8f20df41ae737449c470d5d5e3f
besser <besser9009@mail.ru>
2023-04-04 19:42:27 +0300

    add Num3: search meaning number betwin 1 and N in three degree

---
+++ Num3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+System.Console.Write("Enter meaning N: ");
+int N = int.Parse(Console.ReadLine());
+for (int Z = 1; Z <= N; Z++){
+    System.Console.WriteLine($"{Math.Pow(Z, 3)} ");
+}
